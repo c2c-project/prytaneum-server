d570cd557925f6483960ba04957a12078c944bb7
kadey001 <kadey001@ucr.edu>
2021-01-11 13:14:04 -0600

    added sendEmail with mailgun

--- src/lib/emails.ts
+++ src/lib/emails.ts
@@ -1,4 +1,65 @@
-function sendEmail(...args: any) {}
+import mailgun from 'mailgun-js';
+import env from 'config/env';
+
+const mg = mailgun({
+    apiKey: env.MAILGUN_API_KEY,
+    domain: env.MAILGUN_DOMAIN,
+});
+
+/**
+ * @description internal function to use mg api to send email
+ * @param {string} to email adress being sent to
+ * @param {string} subject subject of the email
+ * @param {string} text body of the email
+ * @param {Date} deliveryTime Time that the email should be sent out (If the date is in the past it will be sent immediately)
+ * @param {string} recipientVariables stringified recipient variables for email template
+ * @param {string} customAPIKey? optional custom API key for mailgun
+ * @param {string} customDomain? optional custom domain for mailgun
+ * @returns {Promise<string | mailgun.messages.SendResponse>}
+ */
+const sendEmail = async (
+    to: string | Array<string>,
+    subject: string,
+    text: string,
+    deliveryTime: Date,
+    recipientVariables: string,
+    customAPIKey?: string,
+    customDomain?: string
+    // eslint-disable-next-line consistent-return
+): Promise<string | mailgun.messages.SendResponse> => {
+    // in development mode, don't send an email, instead we will test this on the staging server
+    // TODO allow user to pass through custom api key and domain
+    let customMailgun: mailgun.Mailgun | undefined;
+    if (customAPIKey && customDomain) {
+        customMailgun = mailgun({ apiKey: customAPIKey, domain: customDomain });
+    }
+    if (env.NODE_ENV === 'test') {
+        return new Promise<string>((resolve) => resolve('success'));
+    }
+    if (env.NODE_ENV === 'development') {
+        return new Promise<string>((resolve) => resolve('success'));
+    }
+    if (customMailgun) {
+        return customMailgun.messages().send({
+            to,
+            from: `Prytaneum <${env.MAILGUN_FROM_EMAIL as string}>`,
+            subject,
+            text,
+            'recipient-variables': recipientVariables,
+            'o:deliverytime': deliveryTime.toUTCString(),
+        });
+    }
+    return mg.messages().send({
+        to,
+        from: `Prytaneum <${env.MAILGUN_FROM_EMAIL as string}>`,
+        subject,
+        text,
+        'recipient-variables': recipientVariables,
+        'o:deliverytime': deliveryTime.toUTCString(),
+    });
+};
+
+// function sendEmail(...args: any) {}
 
 function sendPasswordReset(...args: any) {}
 
@@ -8,4 +69,5 @@ export default {
     sendPasswordReset,
     sendEmail,
     sendEmailVerification,
+    mg
 };
