b236952238992607e402d87239d51a99ad4765e4
Johan Guzman <jguzm022@ucr.edu>
2021-02-05 14:57:55 -0800

    improves feedback modules

--- src/modules/feedback-reports/index.ts
+++ src/modules/feedback-reports/index.ts
@@ -1,8 +1,9 @@
 import { ObjectID } from 'mongodb';
 import type { User } from 'prytaneum-typings';
+import createHttpError from 'http-errors';
 
 import { useCollection } from 'db';
-import { makeMeta } from 'modules/common';
+import { makeMeta, makeUpdatedBy } from 'modules/common';
 
 const numberOfDocumentsPerPage = 10;
 
@@ -14,6 +15,7 @@ const numberOfDocumentsPerPage = 10;
  * @returns MongoDB promise
  */
 export async function createFeedbackReport(description: string, user: User) {
+    const { createdAt, createdBy, updatedAt, updatedBy } = makeMeta(user);
     const { insertedCount } = await useCollection(
         'FeedbackReports',
         (FeedbackReports) =>
@@ -21,7 +23,18 @@ export async function createFeedbackReport(description: string, user: User) {
                 description,
                 resolved: false,
                 replies: [],
-                meta: makeMeta(user), // what's wrong?
+                meta: {
+                    createdAt,
+                    updatedAt,
+                    updatedBy: {
+                        ...updatedBy,
+                        _id: new ObjectID(updatedBy._id),
+                    },
+                    createdBy: {
+                        ...createdBy,
+                        _id: new ObjectID(createdBy._id),
+                    },
+                },
             })
     );
 
@@ -36,71 +49,91 @@ export async function createFeedbackReport(description: string, user: User) {
  * @param {boolean} resolved - Resolved status of reports to retrieve
  * @returns {Promise<FeedbackReport[]>} - Promise that will produce an array of feedback reports
  */
-export function getFeedbackReports(
+export async function getFeedbackReports(
     page: number,
     sortByDate: boolean,
     resolved?: boolean
 ) {
     const resolvedQuery = typeof resolved === 'boolean' ? { resolved } : {};
-
-    return useCollection('FeedbackReports', (FeedbackReports) =>
-        FeedbackReports.find(resolvedQuery)
-            .sort({ date: sortByDate ? 1 : -1 })
-            .skip(page > 0 ? numberOfDocumentsPerPage * (page - 1) : 0)
-            .limit(numberOfDocumentsPerPage)
-            .toArray()
+    const totalCount = await useCollection(
+        'FeedbackReports',
+        (FeedbackReports) => FeedbackReports.countDocuments(resolvedQuery)
     );
+
+    const feedbackReports = await useCollection(
+        'FeedbackReports',
+        (FeedbackReports) =>
+            FeedbackReports.find(resolvedQuery)
+                .sort({ date: sortByDate ? 1 : -1 })
+                .skip(page > 0 ? numberOfDocumentsPerPage * (page - 1) : 0)
+                .limit(numberOfDocumentsPerPage)
+                .toArray()
+    );
+
+    return { feedbackReports, totalCount };
 }
 
 /**
- * @description Retrieves at most 10  feedback reports from a specific submitter, depending on the page number
+ * @description Retrieves at most 10  feedback reports from a specific user, depending on the page number
  * @param {number} page - Page number to return. If the page number exceeds the number of available pages, 0 reports are returned
  * @param {boolean} sortByDate - Sort by date order. True for ascending. False for descending
- * @param {string} submitterId - Id of the submitter
- * @returns {Promise<FeedbackReport[]>} - Promise that will produce an array of feedback reports
+ * @param {string} userId - Id of the user
+ * @returns {Promise} - Promise that will produce an array of feedback reports
  */
-export function getFeedbackReportsByUser(
+export async function getFeedbackReportsByUser(
     page: number,
     sortByDate: boolean,
-    user: User
+    userId: string
 ) {
-    return useCollection('FeedbackReports', (FeedbackReports) =>
-        FeedbackReports.find({
-            'meta.createdBy._id': new ObjectID(user._id),
-        })
-            .sort({ date: sortByDate ? 1 : -1 })
-            .skip(page > 0 ? numberOfDocumentsPerPage * (page - 1) : 0)
-            .limit(numberOfDocumentsPerPage)
-            .toArray()
+    const totalCount = await useCollection(
+        'FeedbackReports',
+        (FeedbackReports) =>
+            FeedbackReports.countDocuments({
+                'meta.createdBy._id': new ObjectID(userId),
+            })
     );
+
+    const feedbackReports = await useCollection(
+        'FeedbackReports',
+        (FeedbackReports) =>
+            FeedbackReports.find({
+                'meta.createdBy._id': new ObjectID(userId),
+            })
+                .sort({ date: sortByDate ? 1 : -1 })
+                .skip(page > 0 ? numberOfDocumentsPerPage * (page - 1) : 0)
+                .limit(numberOfDocumentsPerPage)
+                .toArray()
+    );
+
+    return { feedbackReports, totalCount };
 }
 
 /**
  * @description Retrieves one feedback report by Id
- * @param {string} _id - Id of the feedback report to return
+ * @param {string} reportId - Id of the feedback report to return
  * @returns {Promise<FeedbackReport | null>} - Promise that produces a feedback report or null if no feedback report was found in the collection
  */
-export function getFeedbackReportById(_id: string) {
+export function getFeedbackReportById(reportId: string) {
     return useCollection('FeedbackReports', (FeedbackReports) =>
-        FeedbackReports.findOne({ _id: new ObjectID(_id) })
+        FeedbackReports.findOne({ _id: new ObjectID(reportId) })
     );
 }
 
 /**
  * @description Updates the description of a feedback report specified by its unique Id
- * @param {string} _id - Id of the feedback report to update
+ * @param {string} reportId - Id of the feedback report to update
  * @param {string} newDescription - New description of the feedback report
  * @throws {Error} If unable to update the specified feedback report
  */
 export async function updateFeedbackReport(
-    _id: string,
+    reportId: string,
     newDescription: string
 ) {
     const { upsertedCount } = await useCollection(
         'FeedbackReports',
         (FeedbackReports) =>
             FeedbackReports.updateOne(
-                { _id: new ObjectID(_id) },
+                { _id: new ObjectID(reportId) },
                 { $set: { description: newDescription } }
             )
     );
@@ -111,55 +144,49 @@ export async function updateFeedbackReport(
 
 /**
  * @description Deletes a feedback report by Id
- * @param {string} _id - Id of the feedback report to delete
+ * @param {string} reportId - Id of the feedback report to delete
  */
-export function deleteFeedbackReport(_id: string) {
-    return useCollection('FeedbackReports', (FeedbackReports) =>
-        FeedbackReports.deleteOne({ _id: new ObjectID(_id) })
+export async function deleteFeedbackReport(reportId: string) {
+    const { deletedCount } = await useCollection(
+        'FeedbackReports',
+        (FeedbackReports) =>
+            FeedbackReports.deleteOne({ _id: new ObjectID(reportId) })
     );
+    if (deletedCount === 0)
+        throw createHttpError(404, 'Feedback report not found');
 }
-
-// /**
-//  * @description Returns the total count of reports in the feedback-reports collection
-//  * @param {boolean} resolved Function counts reports that match this resolved status
-//  * @returns total count of feedback reports
-//  */
-// export const getNumberOfFeedbackReports = (
-//     resolved?: boolean
-// ): Promise<number> => {
-//     const resolvedQuery = typeof resolved === 'boolean' ? { resolved } : {};
-//     return Collections.FeedbackReport().countDocuments(resolvedQuery);
-// };
-
-// /**
-//  * @description Returns the count of feedback reports submitted by a specific user
-//  * @param {string} submitterId - Id of user
-//  * @returns count of feedback reports
-//  */
-// export const getNumberOfFeedbackReportsBySubmitter = (
-//     submitterId: string
-// ): Promise<number> => {
-//     return Collections.FeedbackReport().countDocuments({ submitterId });
-// };
 
 /**
  * @description Sets the resolved attribute of a feedback report to the new resolved status provided
- * @param {string} _id - Id of the report
+ * @param {string} reportId - Id of the report
  * @param {boolean} newResolvedStatus - New resolved status
  * @returns Mongodb promise
  * @throws {Error} If unable to update the status of the specified feedback report
  */
 
 export async function updateResolvedStatus(
-    _id: string,
-    newResolvedStatus: boolean
+    reportId: string,
+    newResolvedStatus: boolean,
+    user: User
 ) {
+    const {
+        updatedAt,
+        updatedBy: { _id, name },
+    } = makeUpdatedBy(user);
+
     const { upsertedCount } = await useCollection(
         'FeedbackReports',
         (FeedbackReports) =>
             FeedbackReports.updateOne(
-                { _id: new ObjectID(_id) },
-                { $set: { resolved: newResolvedStatus } }
+                { _id: new ObjectID(reportId) },
+                {
+                    $set: {
+                        resolved: newResolvedStatus,
+                        'meta.updatedAt': updatedAt,
+                        'meta.updatedBy._id': new ObjectID(_id),
+                        'meta.updatedBy.name': name,
+                    },
+                }
             )
     );
 
@@ -170,21 +197,21 @@ export async function updateResolvedStatus(
 /**
  * @description Adds a reply to feedback a report
  * @param {User} user - User object of the replier
- * @param {string} _id - Id of the report
+ * @param {string} reportId - Id of the report
  * @param {string} replyContent - Content of the reply
  * @returns Mongodb promise
  * @throws {Error} If unable to push the new reply to the array of replies of the specified report
  */
 export async function replyToFeedbackReport(
     user: User,
-    _id: string,
+    reportId: string,
     replyContent: string
 ) {
     const { upsertedCount } = await useCollection(
         'FeedbackReports',
         (FeedbackReports) =>
             FeedbackReports.updateOne(
-                { _id: new ObjectID(_id) },
+                { _id: new ObjectID(reportId) },
                 {
                     $push: {
                         replies: {
