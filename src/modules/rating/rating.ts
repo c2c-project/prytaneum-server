2da0cd83a7442ded2462a16f634d88fbf5a631d1
Kelton <kadey001@ucr.edu>
2021-02-11 10:56:35 -0800

    updated filter to use townHallId rather than _id

--- src/modules/rating/rating.ts
+++ src/modules/rating/rating.ts
@@ -8,7 +8,7 @@ const addRating = async (
     rating: RatingForm,
     townhallId: string
 ): Promise<void> => {
-    const filter = { _id: new ObjectId(townhallId) };
+    const filter = { townHallId: new ObjectId(townhallId) };
     const update = { $push: { ratings: rating } };
     const options = { upsert: true, returnOriginal: true };
     const doc = await useCollection('Ratings', (Ratings) => {
