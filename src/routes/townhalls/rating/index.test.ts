dac545756144b56b74d5727b574f64fe09e7305a
Kelton <kadey001@ucr.edu>
2021-02-11 11:18:30 -0800

    updated rating tests

--- src/routes/townhalls/rating/index.test.ts
+++ src/routes/townhalls/rating/index.test.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 import request from 'supertest';
-import { makeRatingForm, makeTownhall, makeUser } from 'prytaneum-typings';
+import { makeRatingForm, makeRating, makeTownhall, makeUser } from 'prytaneum-typings';
 
 import * as DB from 'db/mongo';
 import config from 'config/app';
@@ -26,52 +26,80 @@ const user = makeUser();
 const townhall = makeTownhall();
 
 describe('index', () => {
-    describe('rating', () => {
-        it('should accept valid data', async () => {
-            // spy and mock useCollection
-            const collectionSpy = jest.spyOn(DB, 'useCollection');
-            collectionSpy.mockResolvedValueOnce({});
-
-            const ratingForm = makeRatingForm();
-
-            // make the request
-            const { status } = await request(app)
-                .put(`/${townhall._id}/rating`)
-                .send(ratingForm);
-            expect(status).toStrictEqual(200);
+    describe('ratings', () => {
+        describe('put request', () => {
+            it('should accept valid data', async () => {
+                // spy and mock useCollection
+                const collectionSpy = jest.spyOn(DB, 'useCollection');
+                collectionSpy.mockResolvedValueOnce({});
+    
+                const ratingForm = makeRatingForm();
+    
+                // make the request
+                const { status } = await request(app)
+                    .put(`/${townhall._id}/ratings`)
+                    .send(ratingForm);
+                expect(status).toStrictEqual(200);
+            });
+            it('should accept valid data & userId', async () => {
+                // spy and mock useCollection
+                const collectionSpy = jest.spyOn(DB, 'useCollection');
+                collectionSpy.mockResolvedValueOnce({});
+    
+                const ratingForm = makeRatingForm();
+    
+                // make the request
+                const { status } = await request(app)
+                    .put(`/${townhall._id}/ratings?userId=${user._id}`)
+                    .send(ratingForm);
+                expect(status).toStrictEqual(200);
+            });
+            it('should reject invalid formatted data', async () => {
+                // make the request
+                const { status } = await request(app)
+                    .put(`/${townhall._id}/ratings`)
+                    .send({ invalid: '' });
+                expect(status).toStrictEqual(400);
+            });
+            it('should have status 500 with a database error', async () => {
+                // spy and mock useCollection
+                const collectionSpy = jest.spyOn(DB, 'useCollection');
+                collectionSpy.mockRejectedValue(new Error('Fake Error'));
+    
+                const ratingForm = makeRatingForm();
+    
+                // make the request
+                const { status } = await request(app)
+                    .put(`/${townhall._id}/ratings`)
+                    .send(ratingForm);
+                expect(status).toStrictEqual(500);
+            });
         });
-        it('should accept valid data & userId', async () => {
-            // spy and mock useCollection
-            const collectionSpy = jest.spyOn(DB, 'useCollection');
-            collectionSpy.mockResolvedValueOnce({});
+        describe('get request', () => {
+            it('should accept valid data', async () => {
+                // spy and mock useCollection
+                const collectionSpy = jest.spyOn(DB, 'useCollection');
+                const rating = makeRating();
+                collectionSpy.mockResolvedValueOnce(rating);
 
-            const ratingForm = makeRatingForm();
 
-            // make the request
-            const { status } = await request(app)
-                .put(`/${townhall._id}/rating?userId=${user._id}`)
-                .send(ratingForm);
-            expect(status).toStrictEqual(200);
-        });
-        it('should reject invalid formatted data', async () => {
-            // make the request
-            const { status } = await request(app)
-                .put(`/${townhall._id}/rating`)
-                .send({ invalid: '' });
-            expect(status).toStrictEqual(400);
-        });
-        it('should have status 500 with a database error', async () => {
-            // spy and mock useCollection
-            const collectionSpy = jest.spyOn(DB, 'useCollection');
-            collectionSpy.mockRejectedValue(new Error('Fake Error'));
+                // make the request
+                const { status } = await request(app)
+                    .get(`/${townhall._id}/ratings`)
+                    .send();
+                expect(status).toStrictEqual(200);
+            });
+            it('should have status 500 with a database error', async () => {
+                // spy and mock useCollection
+                const collectionSpy = jest.spyOn(DB, 'useCollection');
+                collectionSpy.mockRejectedValue(new Error('Fake Error'));
 
-            const ratingForm = makeRatingForm();
-
-            // make the request
-            const { status } = await request(app)
-                .put(`/${townhall._id}/rating`)
-                .send(ratingForm);
-            expect(status).toStrictEqual(500);
+                // make the request
+                const { status } = await request(app)
+                    .get(`/${townhall._id}/ratings`)
+                    .send();
+                expect(status).toStrictEqual(500);
+            });
         });
     });
 });
