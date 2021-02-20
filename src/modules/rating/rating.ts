7fdd3bfb0447ecb2e0dd7fbfecc84e47cdfc64aa
Kelton <kadey001@ucr.edu>
2021-02-20 12:56:36 -0800

    updated error throwing and simplified rating query functions.

--- src/modules/rating/rating.ts
+++ src/modules/rating/rating.ts
@@ -2,8 +2,6 @@ import { ObjectId } from 'mongodb';
 import { useCollection } from 'db';
 import type { RatingForm, Rating, Name } from 'prytaneum-typings';
 
-import createHttpError from 'http-errors';
-
 const addRating = async (
     rating: RatingForm,
     townhallId: string,
@@ -23,10 +21,10 @@ const addRating = async (
         ratingDoc.meta.createdBy = { _id: userId, name: userName };
         ratingDoc.meta.updatedBy = { _id: userId, name: userName };
     }
-    const doc = await useCollection('Ratings', (Ratings) => {
+    const result = await useCollection('Ratings', (Ratings) => {
         return Ratings.insertOne(ratingDoc);
     });
-    if (!doc) throw createHttpError(404, 'Error when adding rating');
+    if (result.insertedCount === 0) throw new Error('Unable to add rating');
 };
 
 const getRatings = async (townhallId: string): Promise<Array<Rating<ObjectId>>> => {
@@ -34,7 +32,6 @@ const getRatings = async (townhallId: string): Promise<Array<Rating<ObjectId>>> 
     const cursor = await useCollection('Ratings', (Ratings) => {
         return Ratings.find(filter);
     });
-    if (!cursor || await cursor.count() === 0) throw createHttpError(404, 'Ratings not found');
     const docs = await cursor.toArray();
     return docs;
 };
